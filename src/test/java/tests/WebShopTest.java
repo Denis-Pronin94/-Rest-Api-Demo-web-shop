74faa083a4f9d1e709733e18a7e74634feecf030
dddpr <ddd.pronin@yandex.ru>
2022-06-14 10:20:42 +0300

    added project

---
+++ src/test/java/tests/WebShopTest.java
@@ -0,0 +1,203 @@
+package tests;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.restassured.AllureRestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.netty.channel.group.ChannelMatchers.is;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+
+public class WebShopTest extends TestBase {
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (UI)")
+    void loginTest() {
+        step("Open login page", () ->
+                open("/login"));
+
+        step("Fill login form", () -> {
+            $("#Email").setValue(login);
+            $("#Password").setValue(password)
+                    .pressEnter();
+        });
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+//                .body("Email=" + login + "&Password=" + password + "&RememberMe=false")
+//                .body(format("Email=%s&Password=%s&RememberMe=false", login, password))
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiAndAllureListenerTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(new AllureRestAssured())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiAndCustomListenerTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    };
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful addItemToCartTest in some demowebshop (API + UI)")
+    void addItemToCartTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+        step("ChooseItem", () ->{
+            given()
+                    .log().all()
+                    .when()
+                    .get("/computing-and-internet")
+                    .then()
+                    .log().all()
+                    .statusCode(200);
+        });
+
+        step("Add item to card", () -> {
+            String body = "addtocart_13.EnteredQuantity: 1";
+
+            given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .log().all()
+                    .cookie()
+                    .when()
+                    .post("/addproducttocart/details/13/1")
+                    .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("message", Matchers.is("The product has been added to your " +
+                            "<a href=\"/cart\">shopping cart</a>"));
+        });
+
+        step("Find item in cart", () -> {
+            given()
+                    .log().all()
+                    .cookie()
+                    .when()
+                    .get("/cart")
+                    .then()
+                    .log().all()
+                    .statusCode(200);
+            //                          .body("title", is("Demo Web Shop. Shopping Cart"));
+        });
+    }
+}
